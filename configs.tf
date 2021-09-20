7f1e21ff8f5dfc1126e923f114c4055020ce9062
Alex de Oliveira Delfino <alex.delfino@boavistascpc.com.br>
2021-09-20 18:26:43 -0300

    fist commit

--- configs.tf
+++
@@ -1,4 +0,0 @@
-module "metadata" {
-  source = "gcs::https://www.googleapis.com/storage/v1/bvs-terraform-modules/bvs-global-config-v1.0.11.zip" 
-  tribo  = lower(lookup(var.labels, "value-stream", ))
-}
